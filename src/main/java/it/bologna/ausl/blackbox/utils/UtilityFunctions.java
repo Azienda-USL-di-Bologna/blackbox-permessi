7f88b36c339c5843b5c8bb6cefb53726b4dfab52
Francesco Gusella <gus815@gmail.com>
2021-10-27 15:22:01 +0200

    toloto unused

--- src/main/java/it/bologna/ausl/blackbox/utils/UtilityFunctions.java
+++ src/main/java/it/bologna/ausl/blackbox/utils/UtilityFunctions.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.CaseFormat;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
